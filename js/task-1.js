037d27324ab521f1398e690ee7eb1aa5d8b72daa
Dima_Bezzemianyi <125562777+Bezzemianyi@users.noreply.github.com>
2024-12-05 14:27:20 +0200

    do js,css

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const categoriesList = document.querySelectorAll('ul#categories > li.item');
-console.log(`Number of categories: ${categoriesList.length}`);
-categoriesList.forEach(category => {
-const categoryTitle = category.querySelector('.item-title').textContent;
-const categoryItemsCount = category.querySelectorAll('.item-menu-item').length;
-console.log(`Category: ${categoryTitle}`);
-console.log(`Elements: ${categoryItemsCount}`);
-});
--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.querySelector('#name-input');
-const textOutput = document.querySelector('#name-output')
-textInput.addEventListener('input', (event) => {
-    const inputValue = event.target.value.trim();
-    textOutput.textContent = inputValue || 'Anonymous';
-});
